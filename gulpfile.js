b1a641573c39f2a3daa6393d1b3dd3fc1ed3d987
John Petersson <johnpetersson@gmail.com>
2017-04-27 13:35:13 +0200

    Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require("gulp"),
 livereload = require('gulp-livereload'),
-concat = require('gulp-concat');
+concat = require('gulp-concat'), 
+sass = require('gulp-sass');
+
 
 gulp.task('default', ['watch']);
 
 gulp.task('watch', function() {
 	livereload.listen();
-	gulp.watch('**/*.css', ['reload']);
+	gulp.watch('**/*.scss', ['sass', 'reload']);
 	gulp.watch('**/*.js', ['reload']);
 	gulp.watch('**/*.html', ['reload']);
 	gulp.watch('**/*.php', ['reload']);
@@ -15,3 +17,14 @@ gulp.task('watch', function() {
 gulp.task('reload', function() {
 	livereload.reload();
 }); 
+
+gulp.task('changed', function() {
+	livereload.changed('style.css');
+}); 
+
+gulp.task('sass', function () {
+	console.log("Sass change");
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('.'));
+});
